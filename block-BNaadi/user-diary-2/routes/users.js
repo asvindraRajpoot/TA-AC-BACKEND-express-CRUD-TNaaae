4bc6cd447f3f3999f2585070b70c0be721609d25
asvindra <aswin1399904@gmail.com>
2021-10-26 18:24:29 +0530

    Assignment-2 completed

---
+++ block-BNaadi/user-diary-2/routes/users.js
@@ -0,0 +1,81 @@
+var express=require('express');
+var router=express.Router();
+var User=require('../models/user');
+
+
+// router.get('/new',(req,res)=>{
+
+//     res.render('userForm');
+// })
+
+router.post('/',(req,res,next)=>{
+    console.log(req.body);
+    User.create(req.body,(err,user)=>{
+        if(err)return next(err);
+        console.log(`${user.name} is created`);
+        res.redirect('/users');
+    })
+  
+})
+
+
+
+
+
+router.get('/',(req,res,next)=>{
+    User.find({},(err,users)=>{
+
+        if(err)return next(err);
+        res.render('userList',{users:users});
+    })
+   
+})
+
+
+router.get('/:id',(req,res,next)=>{
+    const id=req.params.id;
+
+
+    User.findById(id,(err,user)=>{
+        if(err)return next(err);
+        res.render('user',{user:user});
+    })
+
+})
+
+
+// router.put('/:id',(req,res,next)=>{
+//     const id=req.params.id;
+
+//   User.findById(id,(err,user)=>{
+//       if(err)return next(err);
+//       res.render('editUser',{user:user});
+//   })
+// })
+
+
+
+
+router.put('/:id',(req,res,next)=>{
+    const id=req.params.id;
+    console.log(id);
+    User.findByIdAndUpdate(id,req.body,(err,updatedUser)=>{
+        if(err)return next(err);
+        res.redirect('/users');
+    })
+
+})
+
+
+router.delete('/:id',(req,res,next)=>{
+    const id=req.params.id;
+    console.log(id);
+  User.findByIdAndDelete(id,(err,user)=>{
+      if(err)return next(err);
+      res.redirect('/users');
+  })
+})
+
+
+
+module.exports=router;
